83fe98e70021564bbbcb449dc38482ce49d7b926
Kiril Christov <kiro.mm@gmail.com>
2020-10-12 11:42:49 -0700

    Updates

---
+++ precache-manifest.2bb1a04fc0545ed543b0a49e3d4631ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed66de9517a2f90bec5cb8cc3bb99b64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3daf2223db816bd809c",
+    "url": "/static/css/2.750b42cb.chunk.css"
+  },
+  {
+    "revision": "2e26ae93687ee0472340",
+    "url": "/static/css/main.4ae0243b.chunk.css"
+  },
+  {
+    "revision": "b3daf2223db816bd809c",
+    "url": "/static/js/2.a3767e87.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/static/js/2.a3767e87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e26ae93687ee0472340",
+    "url": "/static/js/main.437ba8a2.chunk.js"
+  },
+  {
+    "revision": "83b5c123e3c7fc467bf0",
+    "url": "/static/js/runtime-main.bb1c1ccf.js"
+  }
+]);
